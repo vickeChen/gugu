18ec5f81a7123c53bd0dbaffb7a8b560a2887794
pinkDump <1370620378@qq.com>
2018-04-24 18:16:00 +0800

    up
    none

--- gugu2/pages/index/index.js
+++ gugu2/pages/index/index.js
@@ -1,29 +1,41 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var listNum = 0;
+
 var arraylist = []    //任务视图的数组，初始化为空，点击按钮后向其中添加对象
 Page({
   data: {
     addPic: '/pic/add.png',  //添加按钮的未点击状态
     array: '',
     scrollTop: 100,
+    toView: 'task',
+    scrollleft: -200
   
   },
 
   touchAdd: function (e) {  //点击按钮的时候 页面跳转到设置任务界面 希望在设置任务
     var item = {            //界面的参数能够回传以此生成每一个任务的小视图
-      top: 'tasksection',
-      down: 'block'
+     listId: listNum
     }
-    /*
+    
     wx.navigateTo({
-      url: '/pages/task/task'
-    })*/
-    arraylist.push(item)
-    this.setData({
-      addPic: '/pic/addHl.png',
-      array: arraylist
+      url: '/pages/task/task',
+      success: function(res){
+        listNum++;          //task增加
+        arraylist.push(item)  //加入list
+        this.setData({
+          addPic: '/pic/addHl.png',
+          array: arraylist,
+          listId: listNum,
+        })
+      }.bind(this)
     })
+   
+    //console.log(arraylist)
+  },
+  deleteTask: function(){
+    
   },
 
   cancelAdd: function (e) {  //取消点击
@@ -31,28 +43,39 @@ Page({
       addPic: '/pic/add.png'
     })
   },
-  upper: function (e) {  //滑动时候的参数显示
-    console.log(e)
+  lower: function (e) {  //滑动时候的参数显示
+   // console.log(e)
+   setTimeout(function(){    //5秒后回滚
+     this.setData({
+       toView: 'task'
+     })
+    // console.log("run");
+   }.bind(this),5000)
+
   },
-  lower: function (e) {
-    console.log(e)
+  upper: function (e) {
+   // console.log(e)
   },
   scroll: function (e) {
-    console.log(e)
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
-    }
+   // console.log(e)
   },
   tapMove: function (e) {
     this.setData({
       scrollTop: this.data.scrollTop + 10
+    })
+  },
+  deleteTask: function(e){
+    var num = e.target.id     //获取当前view的id
+    console.log(this.data.array);
+    //delete arraylist[num];  //删除id
+    var i = 0;
+    for(; i < this.data.array.length; i++){
+      if(this.data.array[i].listId == parseInt(num))  //找出当前view的index
+      break;
+    }
+    arraylist.splice( i, 1);   //删除当前view
+    this.setData({
+      array: arraylist
     })
   }
 
--- gugu2/pages/task/task.js
+++ gugu2/pages/task/task.js
@@ -1,3 +1,29 @@
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+var timestamp = Date.parse(new Date());
+var adate = new Date(timestamp);
+//年  
+var Y = adate.getFullYear();
+
+//月  
+var M = (adate.getMonth() + 1 < 10 ? '0' + (adate.getMonth() + 1) : adate.getMonth() + 1) ;
+M = parseInt(M) - 1;
+console.log(M);
+//日  
+var D = adate.getDate() < 10 ? '0' + adate.getDate() : adate.getDate() - 1;  
+for (let i = 2010; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
 // pages/task/task.js
 Page({
 
@@ -5,6 +31,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    years: years,
+    year: date.getFullYear(),
+    months: months,
+    month: M,
+    days: days,
+    day: D,
+    value: [9999, M, D],
   
   },
 
@@ -12,7 +45,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+   
   },
 
   okTask: function(){
@@ -23,5 +56,14 @@ Page({
     }  
     })
     
+  },
+  bindChange: function (e) {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]]
+    })
+    console.log(val)
   }
 })
